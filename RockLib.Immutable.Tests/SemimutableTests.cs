61be3e941b501c06ab8be21c0065a91c99172df6
Jason Bock <jason.r.bock@outlook.com>
2022-02-03 15:14:27 -0600

    Updating Libraries to Standards (#4)
    * Initial updates are successful
    
    * Updated .editorconfig with lf, and added a discard for ignored return value.
    
    * Updated NuGet package references
    
    * Updated null checks and added parallelization for tests
    
    Co-authored-by: Jason Bock <jasonbock1@rocketmortgage.com>

--- RockLib.Immutable.Tests/SemimutableTests.cs
+++ RockLib.Immutable.Tests/SemimutableTests.cs
@@ -3,160 +3,160 @@ using Xunit;
 
 namespace RockLib.Immutable.Tests
 {
-    public class SemimutableTests
-    {
-        [Fact]
-        public void DefaultValueIsUsedWhenValueIsNotChanged()
-        {
-            var semimutable = new Semimutable<int>(1);
+   public class SemimutableTests
+   {
+      [Fact]
+      public void DefaultValueIsUsedWhenValueIsNotChanged()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            Assert.Equal(1, semimutable.Value);
-        }
+         Assert.Equal(1, semimutable.Value);
+      }
 
-        [Fact]
-        public void CanChangeValueProperty()
-        {
-            var semimutable = new Semimutable<int>(1);
+      [Fact]
+      public void CanChangeValueProperty()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            semimutable.Value = 2;
+         semimutable.Value = 2;
 
-            Assert.Equal(2, semimutable.Value);
-        }
+         Assert.Equal(2, semimutable.Value);
+      }
 
-        [Fact]
-        public void CanChangeValuePropertyWithTheSetValueMethod()
-        {
-            var semimutable = new Semimutable<int>(1);
+      [Fact]
+      public void CanChangeValuePropertyWithTheSetValueMethod()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            semimutable.SetValue(() => 2);
+         semimutable.SetValue(() => 2);
 
-            Assert.Equal(2, semimutable.Value);
-        }
+         Assert.Equal(2, semimutable.Value);
+      }
 
-        [Fact]
-        public void ResetChangesValueBackToDefault()
-        {
-            var semimutable = new Semimutable<int>(1);
+      [Fact]
+      public void ResetChangesValueBackToDefault()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            semimutable.Value = 2;
+         semimutable.Value = 2;
 
-            semimutable.ResetValue();
+         semimutable.ResetValue();
 
-            Assert.True(semimutable.HasDefaultValue);
-        }
+         Assert.True(semimutable.HasDefaultValue);
+      }
 
-        [Fact]
-        public void HasDefaultValueIsTrueWhenValueIsNotChanged()
-        {
-            var semimutable = new Semimutable<int>(1);
+      [Fact]
+      public void HasDefaultValueIsTrueWhenValueIsNotChanged()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            Assert.True(semimutable.HasDefaultValue);
-        }
+         Assert.True(semimutable.HasDefaultValue);
+      }
 
-        [Fact]
-        public void HasDefaultValueIsFalseWhenValueIsChanged()
-        {
-            var semimutable = new Semimutable<int>(1);
+      [Fact]
+      public void HasDefaultValueIsFalseWhenValueIsChanged()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            semimutable.Value = 2;
+         semimutable.Value = 2;
 
-            Assert.False(semimutable.HasDefaultValue);
-        }
+         Assert.False(semimutable.HasDefaultValue);
+      }
 
-        [Fact]
-        public void HasDefaultValueIsFalseWhenSetValueIsCalled()
-        {
-            var semimutable = new Semimutable<int>(1);
+      [Fact]
+      public void HasDefaultValueIsFalseWhenSetValueIsCalled()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            semimutable.SetValue(() => 2);
+         semimutable.SetValue(() => 2);
 
-            Assert.False(semimutable.HasDefaultValue);
-        }
+         Assert.False(semimutable.HasDefaultValue);
+      }
 
-        [Fact]
-        public void HasDefaultValueIsTrueWhenResetValueIsCalled()
-        {
-            var semimutable = new Semimutable<int>(1);
+      [Fact]
+      public void HasDefaultValueIsTrueWhenResetValueIsCalled()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            semimutable.Value = 2;
-            semimutable.ResetValue();
+         semimutable.Value = 2;
+         semimutable.ResetValue();
 
-            Assert.True(semimutable.HasDefaultValue);
-        }
+         Assert.True(semimutable.HasDefaultValue);
+      }
 
-        [Fact]
-        public void IsLockedIsFalseInitially()
-        {
-            var semimutable = new Semimutable<int>(1);
+      [Fact]
+      public void IsLockedIsFalseInitially()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            Assert.False(semimutable.IsLocked);
-        }
+         Assert.False(semimutable.IsLocked);
+      }
 
-        [Fact]
-        public void IsLockedIsTrueAfterTheValuePropertyIsAccessed()
-        {
-            var semimutable = new Semimutable<int>(1);
+      [Fact]
+      public void IsLockedIsTrueAfterTheValuePropertyIsAccessed()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            var value = semimutable.Value;
+         _ = semimutable.Value;
 
-            Assert.True(semimutable.IsLocked);
-        }
+         Assert.True(semimutable.IsLocked);
+      }
 
-        [Fact]
-        public void IsLockedIsTrueAfterLockValueIsCalled()
-        {
-            var semimutable = new Semimutable<int>(1);
+      [Fact]
+      public void IsLockedIsTrueAfterLockValueIsCalled()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            semimutable.LockValue();
+         semimutable.LockValue();
 
-            Assert.True(semimutable.IsLocked);
-        }
+         Assert.True(semimutable.IsLocked);
+      }
 
-        [Fact]
-        public void SettingTheValuePropertyThrowsWhenIsLockedIsTrue()
-        {
-            var semimutable = new Semimutable<int>(1);
+      [Fact]
+      public void SettingTheValuePropertyThrowsWhenIsLockedIsTrue()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            semimutable.LockValue();
+         semimutable.LockValue();
 
-            Assert.Throws<InvalidOperationException>(() => semimutable.Value = 2);
-        }
+         Assert.Throws<InvalidOperationException>(() => semimutable.Value = 2);
+      }
 
-        [Fact]
-        public void CallingTheSetValueMethodThrowsWhenIsLockedIsTrue()
-        {
-            var semimutable = new Semimutable<int>(1);
+      [Fact]
+      public void CallingTheSetValueMethodThrowsWhenIsLockedIsTrue()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            semimutable.LockValue();
+         semimutable.LockValue();
 
-            Assert.Throws<InvalidOperationException>(() => semimutable.SetValue(() => 2));
-        }
+         Assert.Throws<InvalidOperationException>(() => semimutable.SetValue(() => 2));
+      }
 
-        [Fact]
-        public void CallingTheResetValueMethodThrowsWhenIsLockedIsTrue()
-        {
-            var semimutable = new Semimutable<int>(1);
+      [Fact]
+      public void CallingTheResetValueMethodThrowsWhenIsLockedIsTrue()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            semimutable.LockValue();
+         semimutable.LockValue();
 
-            Assert.Throws<InvalidOperationException>(() => semimutable.ResetValue());
-        }
+         Assert.Throws<InvalidOperationException>(() => semimutable.ResetValue());
+      }
 
-        [Fact]
-        public void InvokingTheMethodReturnedFromGetUnlockMethodUnlocksTheValue()
-        {
-            var semimutable = new Semimutable<int>(1);
+      [Fact]
+      public void InvokingTheMethodReturnedFromGetUnlockMethodUnlocksTheValue()
+      {
+         var semimutable = new Semimutable<int>(1);
 
-            semimutable.LockValue();
+         semimutable.LockValue();
 
-            var unlockValue = semimutable.GetUnlockValueMethod();
-            unlockValue.Invoke(semimutable, null);
+         var unlockValue = semimutable.GetUnlockValueMethod();
+         unlockValue.Invoke(semimutable, null);
 
-            Assert.False(semimutable.IsLocked);
+         Assert.False(semimutable.IsLocked);
 
-            semimutable.Value = 2;
+         semimutable.Value = 2;
 
-            Assert.Equal(2, semimutable.Value);
-        }
-    }
+         Assert.Equal(2, semimutable.Value);
+      }
+   }
 }
